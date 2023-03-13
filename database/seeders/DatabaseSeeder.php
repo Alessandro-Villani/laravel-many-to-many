5a0d082d01639c7e41e7c91303e2db0014ed84e6
Alessandro-Villani <alessvill@libero.it>
2023-03-13 15:44:42 +0100

    added technology seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([UserAdminSeeder::class, ProjectSeeder::class]);
+        $this->call([UserAdminSeeder::class, TypeSeeder::class, TechnologySeeder::class, ProjectSeeder::class]);
     }
 }
